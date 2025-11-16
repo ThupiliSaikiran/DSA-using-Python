0105918d691d2db7e9c0480899287741dcdadb31
ThupiliSaikiran <thupilisaikiran11@gmail.com>
2025-11-16 21:22:55 +0530

    add string

---
+++ String/isIsomorphic.py
@@ -0,0 +1,13 @@
+def isIsomorphic(s,t):
+  n=len(s)
+  mp={}
+  for i in range(n):
+    if s[i] in mp:
+      if mp[s[i]] != t[i]:
+        return False
+    else:
+      mp[s[i]]=t[i]
+  return True
+
+
+print(isIsomorphic("foo","bar"))
